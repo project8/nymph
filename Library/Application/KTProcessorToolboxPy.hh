d25b44991b586f8c6960932c88e37229adb8e7e6
Ben LaRoque <benjamin.laroque@gmail.com>
2017-04-20 18:28:02 -0700

    more toolbox methods

--- Library/Application/KTProcessorToolboxPy.hh
+++ Library/Application/KTProcessorToolboxPy.hh
@@ -11,13 +11,15 @@
 
 #include "KTProcessorToolbox.hh"
 
-//class KTProcessorToolboxWrapper : public KTProcessorToolbox
-//{
-//    public:
-    bool (Nymph::KTProcessorToolbox::*PushBackToRunQueue_string)(const std::string& name) = &Nymph::KTProcessorToolbox::PushBackToRunQueue;
-        //bool (KTProcessorToolboxWrapper::*PushBackToRunQueue_init_list)(std::initializer_list< std::string >) = &KTProcessorToolbox::PushBackToRunQueue;
-        //bool (KTProcessorToolboxWrapper::*PushBackToRunQueue_vector)(std::vector< std::string >) = &KTProcessorToolbox::PushBackToRunQueue;
-//}
+// Make connection overloads
+   /* default values here? */
+bool (Nymph::KTProcessorToolbox::*MakeConnection_3args)(const std::string&, const std::string&, int order) = &Nymph::KTProcessorToolbox::MakeConnection;
+bool (Nymph::KTProcessorToolbox::*MakeConnection_4args)(const std::string&, const std::string&, const std::string&, const std::string&, int order) = &Nymph::KTProcessorToolbox::MakeConnection;
+
+// Run queue pushback overloads
+bool (Nymph::KTProcessorToolbox::*PushBackToRunQueue_string)(const std::string& name) = &Nymph::KTProcessorToolbox::PushBackToRunQueue;
+bool (Nymph::KTProcessorToolbox::*PushBackToRunQueue_init_list)(std::initializer_list< std::string >) = &Nymph::KTProcessorToolbox::PushBackToRunQueue;
+bool (Nymph::KTProcessorToolbox::*PushBackToRunQueue_vector)(std::vector< std::string >) = &Nymph::KTProcessorToolbox::PushBackToRunQueue;
 
 void export_ProcessorToolbox()
 {
@@ -26,65 +28,29 @@ void export_ProcessorToolbox()
     class_<KTProcessorToolbox, boost::noncopyable>("KTProcessorToolbox", init<std::string>())
         .def("Run", &KTProcessorToolbox::Run, "Call Run() on all processors in the run queue")
 
-        // Push new processors to back of run queue
-        .def("PushBackToRunQueue", PushBackToRunQueue_string, "Push a single processor to the back of the run queue")
+        // Not adding Configure methods as they are for conf files, not interactive sessions
+
+        //.def("GetProcessor", &KTProcessorToolbox::GetProcessor, "Get a pointer to a processor in the toolbox")
+        //TODO: what does this ownership statement mean in the context of boost::python? Also doesn't build
+        //.def("AddProcessor", &KTProcessorToolbox::AddProcessor, "add a processor to the toolbox, toolbox takes ownership")
+        .def("RemoveProcessor", &KTProcessorToolbox::RemoveProcessor, "remove a processor from the toolbox")
+        //TODO this next one doesn't build
+        //.def("ReleaseProcessor", &KTProcessorToolbox::ReleaseProcessor, "Remove a processor from the toolbox and return it to the user, ownership is passed") // what does this passed ownership mean in boost::python?
+        .def("ClearProcessors", &KTProcessorToolbox::ClearProcessors, "Remove all processors and clear run queue")
+
+        // make signal-slot connection
+        .def("MakeConnection", MakeConnection_3args, "Make a signal-slot connection")
+        .def("MakeConnection", MakeConnection_4args)
+
+        // Push new processor(s) to back of run queue
+        .def("PushBackToRunQueue", PushBackToRunQueue_string, "Push processor(s) to the back of the run queue")
+        .def("PushBackToRunQueue", PushBackToRunQueue_init_list)
+        .def("PushBackToRunQueue", PushBackToRunQueue_vector)
 
         // Remove items from run queue
         .def("PopBackOfRunQueue", &KTProcessorToolbox::PopBackOfRunQueue, "Remove the last item in the run queue, whether it's a single processor or a group of processors")
         .def("ClearRunQueue", &KTProcessorToolbox::ClearRunQueue, "Clear the run queue")
         ;
 }
-/*
-        public:
-            /// Configure the toolbox: create the processors; connnect signals and slots; and setup the run queue.
-            bool Configure(const scarab::param_node* node);
 
-            /// Configure processors (only those specified in the toolbox)
-            bool ConfigureProcessors(const scarab::param_node* node);
-
-
-        public:
-            /// Get a pointer to a processor in the toolbox
-            KTProcessor* GetProcessor(const std::string& procName);
-            /// Get a pointer to a processor in the toolbox
-            const KTProcessor* GetProcessor(const std::string& procName) const;
-
-            /// Add a processor to the toolbox
-            /// Toolbox takes ownership of the processor
-            bool AddProcessor(const std::string& procName, KTProcessor* proc);
-
-            /// Remove a processor from the toolbox
-            bool RemoveProcessor(const std::string& procName);
-
-            /// Remove a processor from the toolbox and return it to the user
-            /// Ownership is passed to the user
-            KTProcessor* ReleaseProcessor(const std::string& procName);
-
-            /// Remove all processors from the toolbox
-            /// Also clears the run queue
-            void ClearProcessors();
-
-
-        public:
-            /// Make a connection between the signal from one processor and the slot from another processor
-            /// Both processors should already have been added to the Toolbox
-            /// Signal and slot strings should be formatted as: [processor name]:[signal/slot name]
-            bool MakeConnection(const std::string& signal, const std::string& slot, int order = std::numeric_limits< int >::min());
-
-            /// Make a connection between the signal from one processor and the slot from another processor
-            /// Both processors should already have been added to the Toolbox
-            bool MakeConnection(const std::string& signalProcName, const std::string& signalName, const std::string& slotProcName, const std::string& slotName, int order = std::numeric_limits< int >::min());
-
-
-        public:
-            /// Push a single processor to the back of the run queue
-//            bool PushBackToRunQueue(const std::string& name);
-
-            /// Push a set of processors to be run in parallel to the back of the run queue
-            bool PushBackToRunQueue(std::initializer_list< std::string > names);
-            /// Push a set of processors to be run in parallel to the back of the run queue
-            bool PushBackToRunQueue(std::vector< std::string > names);
-
-
-*/
 #endif /* KTPROCESSORTOOLBOXPY_HH_ */
