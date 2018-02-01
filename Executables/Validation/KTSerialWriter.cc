4dd15f41222f888c751568d8b24df146c634865f
Evan Zayas <ezayas@Evans-MacBook-Pro.local>
2018-02-01 12:34:42 -0500

    tabs -> spaces

--- Executables/Validation/KTSerialWriter.cc
+++ Executables/Validation/KTSerialWriter.cc
@@ -1,8 +1,8 @@
 /*
- *	KTSerialWriter.cc
+ *  KTSerialWriter.cc
  *
- *	Created on: Feb 1, 2018
- *		Author: E Zayas
+ *  Created on: Feb 1, 2018
+ *      Author: E Zayas
  *
 */
 
@@ -14,42 +14,42 @@ LOGGER( avlog_hh, "KTSerialWriter" );
 
 namespace Nymph
 {
-	KTSerialWriter::KTSerialWriter( const std::string& name ) :
-		KTProcessor( name ),
-		fFileName( "placeholder.json" ),
-		fStreamOutPtr(nullptr),
-		fArchiveOutPtr(nullptr)
-		{
-			// Register slots here
-		}
+    KTSerialWriter::KTSerialWriter( const std::string& name ) :
+        KTProcessor( name ),
+        fFileName( "placeholder.json" ),
+        fStreamOutPtr(nullptr),
+        fArchiveOutPtr(nullptr)
+        {
+            // Register slots here
+        }
 
-	KTSerialWriter::~KTSerialWriter()
-	{
-		//delete fStreamOutPtr;
-		//delete fArchiveOutPtr;
-	}
+    KTSerialWriter::~KTSerialWriter()
+    {
+        //delete fStreamOutPtr;
+        //delete fArchiveOutPtr;
+    }
 
-	bool KTSerialWriter::Configure( const scarab::param_node* node )
-	{
-		if (node == NULL) return false;
+    bool KTSerialWriter::Configure( const scarab::param_node* node )
+    {
+        if (node == NULL) return false;
 
-		SetFileName( node->get_value< std::string >( "file-name", fFileName ) );
+        SetFileName( node->get_value< std::string >( "file-name", fFileName ) );
 
-		*fStreamOutPtr = std::ofstream( fFileName );
-		cereal::JSONOutputArchive fStupid( *fStreamOutPtr );
-		fArchiveOutPtr = &fStupid;
+        *fStreamOutPtr = std::ofstream( fFileName );
+        cereal::JSONOutputArchive fStupid( *fStreamOutPtr );
+        fArchiveOutPtr = &fStupid;
 
-		return true;
-	}
+        return true;
+    }
 
-	template< class XDataType >
-	void KTSerialWriter::SlotFunction( const XDataType& data )
-	{
+    template< class XDataType >
+    void KTSerialWriter::SlotFunction( const XDataType& data )
+    {
         // Write to JSON archive
         KTINFO( avlog_hh, "Writing data to JSON acrhive" );
         *fArchiveOutPtr( data );
 
         KTINFO( avlog_hh, "Successfully wrote data to archive" );
         return;
-	}
+    }
 }
--- Executables/Validation/KTSerialWriter.hh
+++ Executables/Validation/KTSerialWriter.hh
@@ -1,8 +1,8 @@
 /*
- *	KTSerialWriter.hh
+ *  KTSerialWriter.hh
  *
- *	Created on: Feb 1, 2018
- *		Author: E Zayas
+ *  Created on: Feb 1, 2018
+ *      Author: E Zayas
  *
 */
 
@@ -18,65 +18,65 @@
 
 namespace Nymph
 {
-	class KTSerialWriter : public KTProcessor
-	{
-		public:
-			KTSerialWriter( const std::string& name = "serial-writer" );
-			virtual ~KTSerialWriter();
+    class KTSerialWriter : public KTProcessor
+    {
+        public:
+            KTSerialWriter( const std::string& name = "serial-writer" );
+            virtual ~KTSerialWriter();
 
-		private:
-			std::string fFileName;
-			std::ofstream* fStreamOutPtr;
-			cereal::JSONOutputArchive* fArchiveOutPtr;
+        private:
+            std::string fFileName;
+            std::ofstream* fStreamOutPtr;
+            cereal::JSONOutputArchive* fArchiveOutPtr;
 
-		public:
-			bool Configure( const scarab::param_node* node );
+        public:
+            bool Configure( const scarab::param_node* node );
 
-			std::string GetFileName() const;
-			void SetFileName( std::string file );
+            std::string GetFileName() const;
+            void SetFileName( std::string file );
 
-			std::ofstream* GetStreamOutPtr() const;
-			void SetStreamOutPtr( std::ofstream stream );
+            std::ofstream* GetStreamOutPtr() const;
+            void SetStreamOutPtr( std::ofstream stream );
 
-			cereal::JSONOutputArchive* GetArchiveOutPtr() const;
-			void SetArchiveOutPtr( cereal::JSONOutputArchive archive );
+            cereal::JSONOutputArchive* GetArchiveOutPtr() const;
+            void SetArchiveOutPtr( cereal::JSONOutputArchive archive );
 
-			template< class XDataType >
-			void SlotFunction( const XDataType& data );
-	};
+            template< class XDataType >
+            void SlotFunction( const XDataType& data );
+    };
 
-	inline std::string KTSerialWriter::GetFileName() const
-	{
-		return fFileName;
-	}
+    inline std::string KTSerialWriter::GetFileName() const
+    {
+        return fFileName;
+    }
 
-	inline void KTSerialWriter::SetFileName( std::string file )
-	{
-		fFileName = file;
-		return;
-	}
+    inline void KTSerialWriter::SetFileName( std::string file )
+    {
+        fFileName = file;
+        return;
+    }
 
-	inline std::ofstream* KTSerialWriter::GetStreamOutPtr() const
-	{
-		return fStreamOutPtr;
-	}
+    inline std::ofstream* KTSerialWriter::GetStreamOutPtr() const
+    {
+        return fStreamOutPtr;
+    }
 
-	inline void KTSerialWriter::SetStreamOutPtr( std::ofstream stream )
-	{
-		fStreamOutPtr = &stream;
-		return;
-	}
+    inline void KTSerialWriter::SetStreamOutPtr( std::ofstream stream )
+    {
+        fStreamOutPtr = &stream;
+        return;
+    }
 
-	inline cereal::JSONOutputArchive* KTSerialWriter::GetArchiveOutPtr() const
-	{
-		return fArchiveOutPtr;
-	}
+    inline cereal::JSONOutputArchive* KTSerialWriter::GetArchiveOutPtr() const
+    {
+        return fArchiveOutPtr;
+    }
 
-	inline void KTSerialWriter::SetArchiveOutPtr( cereal::JSONOutputArchive archive )
-	{
-		fArchiveOutPtr = &archive;
-		return;
-	}
+    inline void KTSerialWriter::SetArchiveOutPtr( cereal::JSONOutputArchive archive )
+    {
+        fArchiveOutPtr = &archive;
+        return;
+    }
 
 } // namespace Nymph
 
