0bd645c83c3485d6fc069757e7bcd269aa71a79b
Noah Oblath <nsoblath@gmail.com>
2018-06-25 13:36:30 -0700

    Added initial param_array binding and test functionality

--- Executables/Validation/TestParamBindings.py
+++ Executables/Validation/TestParamBindings.py
@@ -15,6 +15,8 @@ import py_nymph as nymph
 #     9. (self: py_nymph.ParamNodePybind, arg0: str, arg1: float) -> float
 #     10. (self: py_nymph.ParamNodePybind, arg0: str, arg1: str) -> str
 
+
+# Value testing
 int_value = nymph.param_value(5)
 print("What type of value is stored? (bool, uint, int, double, string): ", int_value.is_bool(), int_value.is_uint(), int_value.is_int(), int_value.is_double(), int_value.is_string())
 print("Get value set to 5:", int_value.as_int())
@@ -23,11 +25,8 @@ string_value = nymph.param_value("Hello!")
 print("What type of value is stored? (bool, uint, int, double, string): ", string_value.is_bool(), string_value.is_uint(), string_value.is_int(), string_value.is_double(), string_value.is_string())
 print("Get value set to \"Hello!\":", string_value.as_string())
 
-# print(pnp.get_value())
 
-
-
-
+# Node testing
 a_node = nymph.param_node()
 a_node.add("int-value", int_value)
 a_node.add("string-value", string_value)
@@ -36,4 +35,17 @@ print("Int value from node:", a_node.get_value_int("int-value"))
 print("String value from node:", a_node.get_value_string("string-value"))
 
 # Test the "get_value" function for all the different types: 
-# a_node.get_value("myKey")
+print("Get int value from the node as different types (bool, uint, int, double, string): ", a_node.get_value_bool("int-value"), a_node.get_value_uint("int-value"), a_node.get_value_int("int-value"), a_node.get_value_double("int-value"), a_node.get_value_string("int-value"))
+
+
+# Array testing
+an_array = nymph.param_array()
+an_array.resize(2)
+an_array.assign(0, int_value)
+an_array.assign(1, string_value)
+
+print("Int value from array:", an_array.get_value_int(0))
+print("String value from array:", an_array.get_value_string(1))
+
+# Test the "get_value" function for all the different types: 
+print("Get int value from the array as different types (bool, uint, int, double, string): ", an_array.get_value_bool(0), an_array.get_value_uint(0), an_array.get_value_int(0), an_array.get_value_double(0), an_array.get_value_string(0))
--- Library/Utility/KTParamPybind.hh
+++ Library/Utility/KTParamPybind.hh
@@ -78,6 +78,42 @@ namespace Nymph
             ;
 
         // param_array
+        pybind11::class_< scarab::param_array, scarab::param >( mod, "param_array" )
+            .def( pybind11::init<>() )
+
+            .def( "size", (unsigned (scarab::param_array::*)() const) &scarab::param_array::size,
+                    "Returns the size of the array" )
+
+            .def( "resize", (void (scarab::param_array::*)(unsigned)) &scarab::param_array::resize,
+                    "Sets the size of the array; if smaller than the current size, the extra elements are deleted")
+
+            .def( "assign", (void (scarab::param_array::*)(unsigned, const scarab::param&)) &scarab::param_array::assign,
+                    "Add a param object to the specified index in a array" )
+
+            // Get value of the parameter, when no default is set
+            .def( "get_value_bool", (bool (scarab::param_array::*)(unsigned) const) &scarab::param_array::get_value<bool>,
+                    "Get parameter array value as a bool" )
+            .def( "get_value_uint", (unsigned (scarab::param_array::*)(unsigned) const) &scarab::param_array::get_value<uint>,
+                    "Get parameter array value as an unsigned integer" )
+            .def( "get_value_int", (int (scarab::param_array::*)(unsigned) const) &scarab::param_array::get_value<int>,
+                    "Get parameter array value as a signed integer" )
+            .def( "get_value_double", (double (scarab::param_array::*)(unsigned) const) &scarab::param_array::get_value<double>,
+                    "Get parameter array value as a float" )
+            .def( "get_value_string", (std::string (scarab::param_array::*)(unsigned) const) &scarab::param_array::get_value,
+                    "Get parameter array value as a string" )
+
+            // Get value of the parameter, bringing along the default value
+            .def( "get_value", (bool (scarab::param_array::*)(unsigned, bool) const) &scarab::param_array::get_value<bool>,
+                    "Get parameter array value as a bool" )
+            .def( "get_value", (unsigned (scarab::param_array::*)(unsigned, unsigned) const) &scarab::param_array::get_value<uint>,
+                    "Get parameter array value as an unsigned integer" )
+            .def( "get_value", (int (scarab::param_array::*)(unsigned, int) const) &scarab::param_array::get_value<int>,
+                    "Get parameter array value as a signed integer" )
+            .def( "get_value", (double (scarab::param_array::*)(unsigned, double) const) &scarab::param_array::get_value<double>,
+                    "Get parameter array value as a float" )
+            .def( "get_value", (std::string (scarab::param_array::*)(unsigned, const std::string& ) const) &scarab::param_array::get_value,
+                    "Get parameter array value as a string" )
+            ;
     }
     
 } /* namespace Nymph */
