9448e523666f35cb59f12aabb35bd6cbd5843f27
Noah Oblath <noah.oblath@pnnl.gov>
2021-12-08 15:20:33 -0800

    Move the test processor to its own header file

--- Testing/TestProcessor.cc
+++ Testing/TestProcessor.cc
@@ -5,9 +5,7 @@
  *      Author: N.S. Oblath
  */
 
-#include "Processor.hh"
-#include "Signal.hh"
-#include "Slot.hh"
+#include "TestProc.hh"
 
 #include "logger.hh"
 
@@ -15,61 +13,8 @@
 
 LOGGER( testlog, "TestProcessor" );
 
-namespace Nymph
-{
-    // concrete processor class that we can test
-    // implements Configure() and has its own signal and slot
-    class TestProc : public Processor
-    {
-        public:
-            TestProc() :
-                    Processor( "test" ),
-                    fValue( 0 ),
-                    fSecondValue( 0 ),
-                    fValueSig( "value", this ),
-                    fValueSlot( "value", this, &TestProc::SetValue ),
-                    fSecondValueSig( "second-value", this ),
-                    fSecondValueSlot( "second-value", this, &TestProc::SecondValueSlotFunction, {"second-value"} ),
-                    fSecondValueSlot2( "second-value-2", this, &TestProc::SetSecondValue )
-            {}
+REGISTER_PROCESSOR(Nymph::TestProc, "test-proc");
 
-            virtual ~TestProc()
-            {}
-
-            void Configure( const scarab::param_node& node )
-            {
-                fValue = node.get_value( "value", fValue );
-                return;
-            }
-
-            MEMVAR( int, Value );
-            MEMVAR( int, SecondValue );
-
-            void SecondValueSlotFunction( int newValue )
-            {
-                fSecondValueSig( newValue );
-            }
-
-            MEMVAR_REF( Signal< int >, ValueSig );
-            MEMVAR_REF( Slot< int >, ValueSlot );
-
-            MEMVAR_REF( Signal< int >, SecondValueSig );
-            MEMVAR_REF( Slot< int >, SecondValueSlot );
-            MEMVAR_REF( Slot< int >, SecondValueSlot2 );
-
-    };
-
-    // external slot function owner
-    struct TestSlotOwner
-    {
-        int fValue = 0;
-        void TestSlotFunc( int input )
-        {
-            fValue = input;
-            return;
-        }
-    };
-}
 
 TEST_CASE( "processor", "[signal],[slot],[processor]" )
 {
