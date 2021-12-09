61cbec08b438dcd30f651d37503511e6cef22d1d
Noah Oblath <noah.oblath@pnnl.gov>
2021-12-09 14:21:03 -0800

    Added Connections tests for ProcessorToolbox

--- Testing/TestProcessorToolbox.cc
+++ Testing/TestProcessorToolbox.cc
@@ -26,7 +26,7 @@ TEST_CASE( "processor_toolbox" )
     SECTION( "AddRemoveProcessors" )
     {
         LINFO( testlog, "Add/Remove Processor tests" );
-        
+
         std::string procName1( "testproc-1" );
         std::string procName2( "testproc-2" );
 
@@ -42,7 +42,7 @@ TEST_CASE( "processor_toolbox" )
         scarab::param_translator translator;
         auto config = translator.read_string( config_str, "yaml" );
 
-        toolbox.Configure( config->as_node() );
+        REQUIRE( toolbox.Configure( config->as_node() ) );
 
         REQUIRE( toolbox.GetProcessor( procName1 ) );
         REQUIRE( toolbox.GetProcessor( procName1 )->Name() == procName1 );
@@ -64,5 +64,51 @@ TEST_CASE( "processor_toolbox" )
         REQUIRE_FALSE( toolbox.GetProcessor( procName2 ) );
     }
 
+    SECTION( "Connections" )
+    {
+        LINFO( testlog, "Connections Tests")
+
+        std::string procName1( "testproc-1" );
+        std::string procName2( "testproc-2" );
+
+        std::string config_str(
+            "processors:\n"
+            "- type: test-proc\n"
+            "  name: testproc-1\n"
+            "connections:\n"
+            "- signal: \"testproc-1:value\"\n"
+            "  slot: \"testproc-1:second-value\""
+        );
+
+        scarab::param_translator translator;
+        auto config = translator.read_string( config_str, "yaml" );
+
+        REQUIRE( toolbox.Configure( config->as_node() ) );
+
+        std::shared_ptr< Processor > tp1 = toolbox.GetProcessor( procName1 );
+
+        REQUIRE( tp1->Signals().at("value")->Connections().size() == 1 );
+        REQUIRE( tp1->Slots().at("value")->Connections().size() == 0 );
+        REQUIRE( tp1->Signals().at("second-value")->Connections().size() == 0 );
+        REQUIRE( tp1->Slots().at("second-value")->Connections().size() == 1 );
+
+        tp1->Signals().at("value")->DisconnectAll();
+        REQUIRE( tp1->Signals().at("value")->Connections().empty() );
+
+        toolbox.MakeConnection( "testproc-1:value", "testproc-1:second-value" );
+        REQUIRE( tp1->Signals().at("value")->Connections().size() == 1 );
+        REQUIRE( tp1->Slots().at("value")->Connections().size() == 0 );
+        REQUIRE( tp1->Signals().at("second-value")->Connections().size() == 0 );
+        REQUIRE( tp1->Slots().at("second-value")->Connections().size() == 1 );
+
+        tp1->Signals().at("value")->DisconnectAll();
+        REQUIRE( tp1->Signals().at("value")->Connections().empty() );
+
+        toolbox.MakeConnection( "testproc-1", "value", "testproc-1", "second-value" );
+        REQUIRE( tp1->Signals().at("value")->Connections().size() == 1 );
+        REQUIRE( tp1->Slots().at("value")->Connections().size() == 0 );
+        REQUIRE( tp1->Signals().at("second-value")->Connections().size() == 0 );
+        REQUIRE( tp1->Slots().at("second-value")->Connections().size() == 1 );
+    }
 
 }
