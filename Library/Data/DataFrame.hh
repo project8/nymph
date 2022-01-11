49dc444ca081885aad94adefdf958436af09bea2
Noah Oblath <noah.oblath@pnnl.gov>
2022-01-10 16:14:15 -0800

    Fill out DataFrame a bit

--- Library/Data/DataFrame.hh
+++ Library/Data/DataFrame.hh
@@ -10,8 +10,11 @@
 #define NYMPH_DATAFRAME_HH_
 
 #include "Data.hh"
+#include "Exception.hh"
 
 #include "MemberVariable.hh"
+
+#include "typename.hh"
 
 #include <unordered_map>
 #include <memory>
@@ -19,17 +22,72 @@
 
 namespace Nymph
 {
+    struct DataFrameException : virtual public Exception {};
 
+    /*!
+     @class DataFrame
+
+     @author N. S. Oblath
+
+     @brief Container for Data objects used during data processing
+
+     @details
+     Individual Data objects are held in an unordered map, indexed by type.
+
+    */
     class DataFrame
     {
         public:
             DataFrame();
             virtual ~DataFrame();
 
+            /// Returns true if object of type XData exists in the frame; returns false otherwise
+            template< typename XData >
+            bool Has() const;
+
+            /// Returns a reference to the object of type XData if it exists in the frame.
+            /// Creates and returns a reference to an object of type XData if it does not exist.
+            template< typename XData >
+            XData& Get();
+
+            /// Returns a reference to the object of type XData if it exists in the frame.
+            /// Throws DataFrameException if it does not exist.
+            template< typename XData >
+            const XData& Get() const;
+
             // typedef used to avoid problems with the comma in the MEMVAR macro
             typedef std::unordered_map< std::type_index, std::unique_ptr<Data> > DataMap;
             MEMVAR_REF( DataMap, DataObjects );
     };
+
+
+    template< typename XData >
+    bool DataFrame::Has() const
+    {
+        return fDataObjects.count( typeid(XData) ) != 0;
+    }
+
+    template< typename XData >
+    XData& DataFrame::Get()
+    {
+        if( Has< XData >() )
+        {
+            return static_cast< XData& >( *fDataObjects[typeid(XData)] );
+        }
+
+        fDataObjects[ typeid(XData) ].reset( new XData() );
+        return static_cast< XData& >( *fDataObjects[typeid(XData)] );
+    }
+
+    template< typename XData >
+    const XData& DataFrame::Get() const
+    {
+        if( Has< XData >() )
+        {
+            return static_cast< const XData& >( *fDataObjects.at(typeid(XData) ));
+        }
+        BOOST_THROW_EXCEPTION( DataFrameException() << "Data type <" << scarab::type(XData()) << "> is not present when const Get() was called" << eom );
+    }
 
 } /* namespace Nymph */
 
--- Testing/TestDataFrame.cc
+++ Testing/TestDataFrame.cc
@@ -1,9 +1,11 @@
 /*
  * TestDataFrame.cc
  *
- *  Created on: Jan 5, 2022
+ *  Created on: Jan 6, 2022
  *      Author: N.S. Oblath
  */
+
+#include "TestDataClasses.hh"
 
 #include "DataFrame.hh"
 
@@ -15,6 +17,25 @@ TEST_CASE( "data_frame", "[data]" )
     using namespace Nymph;
 
     DataFrame frame;
-    REQUIRE( frame.DataObjects().empty() );
+    const DataFrame& cFrame = frame;
 
+    REQUIRE( frame.DataObjects().empty() );
+    REQUIRE_FALSE( frame.Has< TestData1 >() );
+    REQUIRE_FALSE( frame.Has< TestData2 >() );
+
+    // create a data object  with Get()
+    TestData1& data1 = frame.Get< TestData1 >();
+    REQUIRE( frame.Has< TestData1 >() );
+    // ensure TestData1 object was initialized correctly
+    REQUIRE( data1.GetIValue1() == 0 );
+    REQUIRE( data1.GetIValue2() == 5 );
+
+    // make sure that we haven't made a copy of the data object
+    data1.SetIValue1( 50 );
+    REQUIRE( data1.GetIValue1() == 50 );
+    REQUIRE( frame.Get< TestData1 >().GetIValue1() == 50 );
+
+    // check const access
+    REQUIRE( cFrame.Get< TestData1 >().GetIValue1() == 50 );
+    REQUIRE_THROWS_AS( cFrame.Get< TestData2 >(), DataFrameException );
 }
