90fda0d3aa05786d1ce9df1db4a428b46ca69037
Noah Oblath <noah.oblath@pnnl.gov>
2022-01-12 16:19:14 -0800

    TestPrimaryProcessor tests multiple run configurations

--- Testing/TestPrimaryProcessor.cc
+++ Testing/TestPrimaryProcessor.cc
@@ -5,7 +5,7 @@
  *      Author: N.S. Oblath
  */
 
-#include "TestProc.hh"
+#include "TestProcessorClasses.hh"
 
 #include "logger.hh"
 
@@ -21,12 +21,33 @@ TEST_CASE( "primary_processor", "[primary_processor]" )
     using namespace Nymph;
 
     TestPrimaryProc tester;
+    REQUIRE_FALSE( tester.ExceptionPtr() );
 
+    // SignalNewValue test
+    tester.SetTestSelection( TestPrimaryProc::TestType::SignalNewValue );
     tester.ConnectASlot( "value", tester, "value" );
-
     tester();
-
     REQUIRE( tester.GetValue() == tester.GetNewValue() );
+
+    // WaitTwoSec test
+    // -- no test implemented here right now --
+    //tester.SetTestSelection( TestPrimaryProc::TestType::WaitTwoSec );
+
+    // ThrowExcept test
+    tester.SetTestSelection( TestPrimaryProc::TestType::ThrowExcept );
+    REQUIRE_NOTHROW( tester() ); // the exception should be caught by operator() and stored as the exception ptr
+    REQUIRE( tester.ExceptionPtr() ); // the exception ptr should now be set
+    try
+    {
+        std::rethrow_exception( tester.ExceptionPtr() );
+    }
+    catch( const scarab::base_exception& e )
+    {
+        PrintException( e );
+        REQUIRE( std::string(e.what()) == std::string("PrimaryProcessor test function: throw Exception") );
+    }
+    
+    
 
     //REQUIRE( tester.ValueSig().GetControl() == &control );
 }
