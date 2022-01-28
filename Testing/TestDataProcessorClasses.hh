5e554a37b7a02dfefd181b66c1053a43765f3c85
Noah Oblath <noah.oblath@pnnl.gov>
2022-01-27 16:03:40 -0800

    Implement TestSlotData

--- Testing/TestDataProcessorClasses.hh
+++ Testing/TestDataProcessorClasses.hh
@@ -7,23 +7,44 @@
 
 #include "TestDataClasses.hh"
 
+#include "Exception.hh"
 #include "Processor.hh"
 #include "SignalData.hh"
 #include "SlotData.hh"
 
+#include "logger.hh"
+
+LOGGER( tdpclog_h, "TestDataProcessorClasses" );
 
 namespace Nymph
 {
-    // This processor has a data slot that takes TestData1 as input and has no output
+
+    // external slot function owner
+    struct MultiplyFuncOwner
+    {
+        double fValue = 2.5;
+        void Multiply( const TestData1& intData, TestData2& doubleData )
+        {
+            doubleData.SetDValue1( (double)intData.GetIValue1() * fValue );
+            doubleData.SetDValue2( (double)intData.GetIValue2() * fValue );
+            return;
+        }
+    };
+
+    // This processor has a data slot that takes TestData1 as input and has no output (TestData1 is modified in place)
     class Adder : public Processor
     {
         public:
             Adder( const std::string& name = "adder" ) :
                     Processor( name ),
-                    fValue( 0 ),
+                    fAddValue( 0 ),
                     fAddSignal( "add", this ),
                     fAddSlotWithSig( "add-sig", this, &Adder::Add, &fAddSignal ),
-                    fAddSlotNoSig( "add-no-sig", this, &Adder::Add )
+                    fAddSlotNoSig( "add-no-sig", this, &Adder::Add ),
+                    fMultiplySlot( "multiply", this, &fMultFunc, &MultiplyFuncOwner::Multiply ),
+                    fIToDSlot( "i-to-d", this, &Adder::IntToDouble ),
+                    fPrintSlot( "print", this, &Adder::Print ),
+                    fJustThrowsSlot( "just-throws", this, &Adder::JustThrows )
             {}
 
             virtual ~Adder()
@@ -31,26 +52,58 @@ namespace Nymph
 
             void Configure( const scarab::param_node& node )
             {
-                fValue = node.get_value( "value", fValue );
+                fAddValue = node.get_value( "to-add", fAddValue );
+
+                fMultFunc.fValue = node.get_value( "to-mult", fMultFunc.fValue );
 
                 return;
             }
 
-            MEMVAR( int, Value );
+            MEMVAR( int, AddValue );
 
         public:
             void Add( TestData1& data )
             {
-                data.SetIValue1( data.GetIValue1() + fValue );
-                data.SetIValue2( data.GetIValue2() + fValue );
+                data.SetIValue1( data.GetIValue1() + fAddValue );
+                data.SetIValue2( data.GetIValue2() + fAddValue );
                 return;
             }
+
+            void Print( const TestData1& data )
+            {
+                LPROG( tdpclog_h, "Value 1: " << data.GetIValue1() );
+                LPROG( tdpclog_h, "Value 2: " << data.GetIValue2() );
+                return;
+            }
+
+            void IntToDouble( const TestData1& intData, TestData2& doubleData )
+            {
+                doubleData.SetDValue1( (double)intData.GetIValue1() );
+                doubleData.SetDValue2( (double)intData.GetIValue2() );
+                return;
+            }
+
+            void JustThrows( TestData1& )
+            {
+                throw CREATE_EXCEPT_HERE( Exception );
+            }
+
+        protected:
+            MultiplyFuncOwner fMultFunc;
 
         public:
             SignalData fAddSignal;
 
             SlotData< In<TestData1>, Out<> > fAddSlotWithSig;
             SlotData< In<TestData1>, Out<> > fAddSlotNoSig;
+
+            SlotData< In<const TestData1>, Out<TestData2> > fMultiplySlot;
+
+            SlotData< In<const TestData1>, Out<TestData2> > fIToDSlot;
+
+            SlotData< In<const TestData1>, Out<> > fPrintSlot;
+
+            SlotData< In<TestData1>, Out<> > fJustThrowsSlot;
 
     };
 
--- Testing/TestSlotData.cc
+++ Testing/TestSlotData.cc
@@ -5,32 +5,116 @@
  *      Author: N.S. Oblath
  */
 
+#include "TestControllerClasses.hh"
 #include "TestDataProcessorClasses.hh"
 
 #include "catch.hpp"
 
-TEST_CASE( "signal_slot", "[slot],[data]" )
+TEST_CASE( "slot_data", "[slot],[data]" )
 {
     using namespace Nymph;
 
+    // need a controller to exist
+    CIQThrowController controller;
+
+
 // TODO: test creation of slot with:
-//   * proc == owner; no return; multiple inputs
-//   * proc == owner; multiple returns; multiple inputs
-//   * proc != owner; no return; multiple inputs
+//   x proc == owner; no return; multiple inputs
+//   x proc == owner; multiple returns; multiple inputs
 //   * proc= != owner; multiple returns; multiple inputs
+//   x const arguments (supply const with type)
 
 // TODO: test calling of function
-//   * multiple inputs; data not present; throws exception
-//   * multiple inputs; multiple output; call is successful
-//   * call throws
+//   x multiple inputs; data not present; throws Exception
+//   x multiple inputs; multiple output; call is successful
+//   x call throws results in thrown Exception
 
 // TODO: test signal emmission
+//   x slot with output calls a second slot
 
+    // create the processor and verify the presence of the slots
     Adder adder;
-    adder.SetValue( 5 );
+    REQUIRE( adder.Slots().count("add-sig") == 1 );
+    REQUIRE( adder.Slots().count("add-no-sig") == 1 );
 
+    // get signal and slot pointers and cast
+    SlotData< In<TestData1>, Out<> >* sdAddSig = dynamic_cast< SlotData< In<TestData1>, Out<> >* >( adder.Slots().at("add-sig") );
+    SlotData< In<TestData1>, Out<> >* sdAddNoSig = dynamic_cast< SlotData< In<TestData1>, Out<> >* >( adder.Slots().at("add-no-sig") );
+    REQUIRE( sdAddSig );
+    REQUIRE( sdAddNoSig );
 
+    SlotData< In<const TestData1>, Out<TestData2> >* sdMult = dynamic_cast< SlotData< In<const TestData1>, Out<TestData2> >* >( adder.Slots().at("multiply") );
+    REQUIRE( sdMult );
 
+    SlotData< In<const TestData1>, Out<TestData2> >* sdIToD = dynamic_cast< SlotData< In<const TestData1>, Out<TestData2> >* >( adder.Slots().at("i-to-d") );
+    REQUIRE( sdIToD );
+
+    SlotData< In<const TestData1>, Out<> >* sdPrint = dynamic_cast< SlotData< In<const TestData1>, Out<> >* >( adder.Slots().at("print") );
+    REQUIRE( sdPrint );
+
+    SlotData< In<TestData1>, Out<> >* sdJustThrows = dynamic_cast< SlotData< In<TestData1>, Out<> >* >( adder.Slots().at("just-throws") );
+    REQUIRE( sdJustThrows );
+
+    SignalData* sigdAdd = dynamic_cast< SignalData* >( adder.Signals().at("add") );
+    REQUIRE( sigdAdd );
+
+    // configure the processor
+    adder.SetAddValue( 5 );
+
+    // create a data frame
+    DataHandle handle = std::make_shared< DataFrame >();
+
+    // verify that when we call the slot on an empty frame we get an exception
+    REQUIRE_THROWS_AS( (*sdAddSig)( handle ), Exception );
+
+    TestData1& td1 = handle->Get< TestData1 >();
+    td1.SetIValue1( 1 );
+    td1.SetIValue2( 2 );
+
+    // verify that slots can be called, n this case with an input and no output
+    // verify that slots work whether or not signals are automatically called
+    // call the add slot functions and verify operation
+    REQUIRE_NOTHROW( (*sdAddSig)( handle ) );
+    REQUIRE( td1.GetIValue1() == 6 );
+    REQUIRE( td1.GetIValue2() == 7 );
+
+    REQUIRE_NOTHROW( (*sdAddNoSig)( handle ) );
+    REQUIRE( td1.GetIValue1() == 11 );
+
+    // call the IToD slot function and verify operation
+    REQUIRE_NOTHROW( (*sdIToD)( handle ) );
+    REQUIRE( handle->Has< TestData2 >() );
+    TestData2& td2 = handle->Get< TestData2 >();
+    REQUIRE( td2.GetDValue1() == Approx(11.) );
+    REQUIRE( td2.GetDValue2() == Approx(12.) );
+
+    // verify that a slot with a const data object works
+    // call the print slot function to verify use with const data
+    REQUIRE_NOTHROW( (*sdPrint)( handle ) );
+
+    // verify that a thrown exception appears
+    // calls the slot function that just throws Exception; verify it appears here
+    REQUIRE_THROWS_AS( (*sdJustThrows)( handle ), Exception );
+
+    // verify that an automatic signal call is made
+    // connect signal "add" to slot "i-to-d"
+    REQUIRE_NOTHROW( adder.ConnectSignalToSlot( sigdAdd, sdIToD ) );
+    // remove TestData2
+    handle->Remove< TestData2 >();
+    REQUIRE_FALSE( handle->Has< TestData2 >() );
+    // call the add-sig slot and verify that the i-to-d slot gets called via the signal
+    REQUIRE_NOTHROW( (*sdAddSig)( handle ) );
+    REQUIRE( handle->Has< TestData2 >() );
+    REQUIRE( td1.GetIValue1() == 16 );
+    REQUIRE( td2.GetDValue1() == Approx(16.) );
+
+    // verify that slots owned by something else work
+    // remove TestData2
+    handle->Remove< TestData2 >();
+    REQUIRE_FALSE( handle->Has< TestData2 >() );
+    // call the mult slot and verify that it worked
+    REQUIRE_NOTHROW( (*sdMult)( handle ) );
+    REQUIRE( td2.GetDValue1() == Approx(40.) );
 }
 
 
