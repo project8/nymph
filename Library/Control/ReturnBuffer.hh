65d080bd2f649a61a082b6e1b3bb9d1bcf92dd7c
Noah Oblath <noah.oblath@pnnl.gov>
2022-02-01 11:03:38 -0800

    Updated ReturnBuffer.hh and added TestReturnBuffer.cc

--- Library/Control/ReturnBuffer.hh
+++ Library/Control/ReturnBuffer.hh
@@ -8,6 +8,8 @@
 #ifndef NYMPH_RETURNBUFFER_HH_
 #define NYMPH_RETURNBUFFER_HH_
 
+#include "Exception.hh"
+
 #include <tuple>
 
 namespace Nymph
@@ -21,49 +23,69 @@ namespace Nymph
 
             template< class... XArgs >
             std::tuple< XArgs&... >& GetReturn();
+
+            template< class... XArgs >
+            const std::tuple< XArgs&... >& GetReturn() const;
     };
-
-    template< typename... Args >
-    struct ReturnBuffer : ReturnBufferBase
-    {
-        public:
-            ReturnBuffer() :
-                    fReturn( nullptr )
-            {}
-
-            ReturnBuffer( Args&... retval ) :
-                    fReturn( new std::tuple< Args&... >( retval... ) )
-            {}
-
-            virtual ~ReturnBuffer()
-            {}
-
-            std::tuple< Args&... >& GetReturn()
-            {
-                if( fReturn == nullptr ) throw std::exception();
-                return *fReturn;
-            }
-
-        protected:
-            std::tuple< Args&... >* fReturn;
-    };
-
-    template<>
-    struct ReturnBuffer< void > : ReturnBufferBase
-    {
-        ReturnBuffer() :
-                ReturnBufferBase()
-        {}
-
-        void GetReturn()
-        {
-            return;
-        }
-    };
-
 
     template< class... XArgs >
+    class ReturnBuffer : public ReturnBufferBase
+    {
+        public:
+            ReturnBuffer();
+            ReturnBuffer( XArgs&... retval );
+            virtual ~ReturnBuffer();
+
+            std::tuple< XArgs&... >& GetReturn();
+
+            const std::tuple< XArgs&... >& GetReturn() const;
+
+        protected:
+            std::tuple< XArgs&... >* fReturn;
+    };
+
+
+    // ReturnBuffer
+    template< class... XArgs >
+    ReturnBuffer< XArgs... >::ReturnBuffer() :
+            fReturn( nullptr )
+    {}
+
+    template< class... XArgs >
+    ReturnBuffer< XArgs... >::ReturnBuffer( XArgs&... retval ) :
+            fReturn( new std::tuple< XArgs&... >( retval... ) )
+    {}
+
+    template< class... XArgs >
+    ReturnBuffer< XArgs... >::~ReturnBuffer()
+    {}
+
+    template< class... XArgs >
+    std::tuple< XArgs&... >& ReturnBuffer< XArgs... >::GetReturn()
+    {
+        if( fReturn == nullptr ) THROW_EXCEPT_HERE( Exception() << "No return is present" );
+        return *fReturn;
+    }
+
+    template< class... XArgs >
+    const std::tuple< XArgs&... >& ReturnBuffer< XArgs... >::GetReturn() const
+    {
+        if( fReturn == nullptr ) THROW_EXCEPT_HERE( Exception() << "No return is present" );
+        return *fReturn;
+    }
+
+
+    // Templated functions from ReturnBufferBase
+    template< class... XArgs >
     std::tuple< XArgs&... >& ReturnBufferBase::GetReturn()
+    {
+        ReturnBuffer< XArgs... >* typedBuffer = dynamic_cast< ReturnBuffer< XArgs... >* >(this);
+        if( ! typedBuffer ) THROW_EXCEPT_HERE( Exception() << "Invalid set of arguments for this return buffer" );
+        return typedBuffer->GetReturn();
+    }
+
+    template< class... XArgs >
+    const std::tuple< XArgs&... >& ReturnBufferBase::GetReturn() const
     {
         ReturnBuffer< XArgs... >* typedBuffer = dynamic_cast< ReturnBuffer< XArgs... >* >(this);
         if( ! typedBuffer ) THROW_EXCEPT_HERE( Exception() << "Invalid set of arguments for this return buffer" );
