35bd354b8d6ab77c4e8117786c33296e46a7fd84
Florian Thomas <flthomas@students.uni-mainz.de>
2022-04-05 13:30:10 -0400

    Modify binding code for Processor class
    Adds the required trampoline class. Code compiles now.

--- Python/Bindings/Processor/ProcessorPybind.hh
+++ Python/Bindings/Processor/ProcessorPybind.hh
@@ -16,10 +16,26 @@ namespace py = pybind11;
 
 namespace NymphPybind
 {
+	//trampoline class
+	class PyProcessor : public Nymph::Processor {
+		public:
+			/* Inherit the constructors */
+			using Nymph::Processor::Processor;
+
+			/* Trampoline (need one for each virtual function) */
+			void Configure( const scarab::param_node& node ) override {
+				PYBIND11_OVERRIDE_PURE(
+					void, /* Return type */
+					Processor,      /* Parent class */
+					Configure,          /* Name of function in C++ (must match Python name) */
+					node/* Argument(s) */
+				);
+			}
+	};
 
 	void ExportProcessor( py::module_& nymphProcessor)
 	{
-	    py::class_< Nymph::Processor>(nymphProcessor, "_Processor")
+	    py::class_< Nymph::Processor, PyProcessor>(nymphProcessor, "_Processor")
 	    		.def(py::init<const std::string& >())
 				.def("configure", &Nymph::Processor::Configure)
 				.def("connect_signal_to_slot", &Nymph::Processor::ConnectSignalToSlot)
@@ -28,8 +44,8 @@ namespace NymphPybind
 				.def("register_signal", &Nymph::Processor::RegisterSignal)
 				.def("register_slot", &Nymph::Processor::RegisterSlot)
 				.def("get_do_breakpoint", &Nymph::Processor::GetDoBreakpoint)
-				.def("set_do_breakpoint", &Nymph::Processor::SetDoBreakpoint)
-				.def_property_readonly("name", &Nymph::Processor::Name);
+				.def("set_do_breakpoint", &Nymph::Processor::SetDoBreakpoint);
+				//.def_property_readonly("name", &Nymph::Processor::Name);
 	}
 
 }
