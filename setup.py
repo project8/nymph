10890e4813ff7688481cca2587f331411ed4fa08
Andrew Ziegler <ziegler@HAL.local>
2022-05-04 08:15:43 -0400

    modified the cmake args for he python build

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ class CMakeBuild(build_ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
                       '-DPYTHON_EXECUTABLE=' + sys.executable,
-                      '-DCMAKE_INSTALL_PREFIX:PATH=/usr/local',
+                      #'-DCMAKE_INSTALL_PREFIX:PATH=/usr/local',
                       '-DNymph_ENABLE_PYTHON:bool=1',
                      ]
 
