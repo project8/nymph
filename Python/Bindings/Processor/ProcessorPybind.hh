aa161bab0b00435813bd897a48eb2e4b4dab5d75
Florian Thomas <flthomas@students.uni-mainz.de>
2022-05-30 18:43:48 -0400

    Add function to python module to get existing processors

--- Python/Bindings/Processor/ProcessorPybind.hh
+++ Python/Bindings/Processor/ProcessorPybind.hh
@@ -9,8 +9,10 @@
 #define PYTHON_BINDINGS_PROCESSOR_PROCESSORPYBIND_HH_
 
 #include <pybind11/pybind11.h>
+#include <memory>
 
 #include "Processor.hh"
+#include "factory.hh"
 
 namespace py = pybind11;
 
@@ -32,10 +34,16 @@ namespace NymphPybind
 				);
 			}
 	};
+	
+	std::shared_ptr< Nymph::Processor > get_processor(const std::string& type, const std::string& name)
+	{
+		scarab::factory< Nymph::Processor, const std::string& >* factory = scarab::factory< Nymph::Processor, const std::string& >::get_instance();
+		return std::shared_ptr< Nymph::Processor >(factory->create(type, name));
+	}
 
 	void ExportProcessor( py::module_& nymphProcessor)
 	{
-	    py::class_< Nymph::Processor, PyProcessor>(nymphProcessor, "_Processor")
+	    py::class_< Nymph::Processor, PyProcessor, std::shared_ptr<Nymph::Processor> >(nymphProcessor, "_Processor")
 	    		.def(py::init<const std::string& >())
 				.def("configure", &Nymph::Processor::Configure)
 				.def("connect_signal_to_slot", &Nymph::Processor::ConnectSignalToSlot)
@@ -46,6 +54,8 @@ namespace NymphPybind
 				.def("get_do_breakpoint", &Nymph::Processor::GetDoBreakpoint)
 				.def("set_do_breakpoint", &Nymph::Processor::SetDoBreakpoint);
 				//.def_property_readonly("name", &Nymph::Processor::Name);
+		
+		nymphProcessor.def("get_processor", &get_processor);
 	}
 
 }
