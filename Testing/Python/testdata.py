c96a529573e30116b5e2471f17c9c62e7f371825
Florian Thomas <flthomas@students.uni-mainz.de>
2022-06-13 13:27:23 -0400

    Add small demo for python binding of nymph Data

---
+++ Testing/Python/testdata.py
@@ -0,0 +1,28 @@
+
+"""
+  testdata.py
+ 
+   Created on: Jun 13, 2022
+       Author: F. Thomas
+"""
+
+__all__ = []
+
+import _nymph
+
+class TestData(_nymph.data._Data):
+    
+    def __init__(self, test_var):
+        super().__init__()
+        self.test_var = test_var
+
+def main(args):
+    
+    test_data = TestData('foo')
+    
+    print(test_data.test_var)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
