e9cfa5d6dff9c7cc717d2b95a90101a66c77f37c
Florian Thomas <flthomas@students.uni-mainz.de>
2022-06-14 16:28:57 -0400

    Fix build bug?

--- Python/Bindings/Processor/SignalPybind.hh
+++ Python/Bindings/Processor/SignalPybind.hh
@@ -13,19 +13,27 @@
 
 #include "SignalSlotBase.hh"
 #include "Signal.hh"
+#include "Slot.hh"
 #include "Processor.hh"
 
 namespace py = pybind11;
 
 namespace NymphPybind
 {
+    
+    template< typename... XArgs >
+    void explicit_name_setter(Nymph::Signal< XArgs... >& signal)
+    {
+        
+    }
 
     template< typename... XArgs >
     void ExportSignal( py::module_& nymphProcessor, const std::string& typestr)
     {
         std::string pyClsName = std::string("_Signal") + typestr;
         
-        py::class_< Nymph::Signal< XArgs... >, std::shared_ptr<Nymph::Signal< XArgs... > > >(nymphProcessor, pyClsName.c_str())
+       // py::class_< Nymph::Signal< XArgs... >, std::shared_ptr<Nymph::Signal< XArgs... > > >(nymphProcessor, pyClsName.c_str())
+       py::class_< Nymph::Signal< XArgs... >, std::shared_ptr<Nymph::Signal< XArgs... > > >(nymphProcessor, pyClsName.c_str())
                 .def(py::init<const std::string& >())
                 .def(py::init<const std::string&,  Nymph::Processor* >())
                 .def("emit", &Nymph::Signal< XArgs... >::Emit)
@@ -33,12 +41,10 @@ namespace NymphPybind
                 .def("connect", &Nymph::Signal< XArgs...>::Connect)
                 .def("disconnect", &Nymph::Signal< XArgs... >::Disconnect)
                 .def("disconnect_all", &Nymph::Signal< XArgs... >::DisconnectAll)
-             //   .def("add_connection", &Nymph::Signal< XArgs... >::AddConnection) <- private=cannot be used by signal
-                //.def_property_readonly("connections", &Nymph::Signal< XArgs... >::Connections)
-             //   .def_property_readonly("connections", static_cast<const std::set< Nymph::SlotBase* > (Nymph::Signal< XArgs... >::*)() const>(&Nymph::Signal< XArgs... >::Connections))
-                //.def("set", static_cast<void (Pet::*)(const std::string &)>(&Pet::set), "Set the pet's name");
+               // .def_property_readonly("connections", static_cast<const std::set< Nymph::SlotBase* >& (Nymph::Signal< XArgs... >::*)() const>(&Nymph::Signal< XArgs... >::Connections))
+                //.def_property("name", static_cast< const std::string& (Nymph::Signal< XArgs... >::*)() const>(&Nymph::Signal< XArgs... >::Name),
+                //                      static_cast< const std::string& (Nymph::Signal< XArgs... >::*)() const>(&Nymph::Signal< XArgs... >::Name) )
                 .def_property("do_breakpoint", &Nymph::Signal< XArgs... >::GetDoBreakpoint, &Nymph::Signal< XArgs... >::SetDoBreakpoint);
-             //   .def_readwrite("name", &Nymph::Signal< XArgs... >::fName)
         
     }
 
