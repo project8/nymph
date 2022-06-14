8644d6e7f1ae48a73846b0069ee2eee3b21c2a69
jkgaison65 <jkgaison65@gmail.com>
2022-06-14 12:12:21 -0400

    reading config file from CL, initially working

--- Cpp/Executables/Main/Nymph_Exe.cc
+++ Cpp/Executables/Main/Nymph_Exe.cc
@@ -15,7 +15,7 @@
 
 #include "TestControllerClasses.hh"
 #include "TestProcessorClasses.hh"
-//#include "param_codec.hh"
+#include "param_codec.hh"
 #include "signal_handler.hh"
 #include "application.hh"
 
@@ -24,6 +24,7 @@ LOGGER( nymphlog, "Nymph" );
 int main( int argc, char** argv )
 {
 
+    // Create the application
     REGISTER_PROCESSOR( Nymph, TestPrimaryProc, "test-primary" );
 
     LPROG( nymphlog, "Welcome to Nymph!" );
@@ -65,7 +66,9 @@ int main( int argc, char** argv )
 
     try
     {
-        /*std::string config_str(
+//-----------------Hard Coded Config Test-----------------
+/*
+        std::string config_str(
             "processors:\n"
             "- type: test-primary\n"
             "  name: pp\n"
@@ -74,35 +77,54 @@ int main( int argc, char** argv )
             "  slot: \"pp:value\"\n"
             "run-queue:\n"
             "- pp\n"
-        );*/
+        );
+	scarab::param_translator translator;
+	auto config_test = translator.read_string( config_str, "yaml" );
+
+        return Nymph::RunNymph( config_test->as_node() );
+*/
+//----------------CLI Config Test-------------------
 
     // Start handling signals
-//    scarab::signal_handler t_sig_hand;
+    scarab::signal_handler t_sig_hand;
 
     // Create the application
-//    scarab::main_app the_main;
+    scarab::main_app the_main;
+    the_main.set_global_verbosity(scarab::logger::ELevel::eDebug);
 
     // Default configuration
 //    the_main.default_config() =
 
-    // Package version
-//    the_main.set_version( version_store::get_instance()->versions().at("project8/nymph") );
-
     int the_return = -1;
-//    auto t_callback = [&](){
-        
-//    }
+/*
+    auto t_callback = [&](){
+//	the_return = dl_client_error().rc_value() / 100;
 
-//    the_main.callback( t_callback );
+	auto the_monitor = std::make_shared< monitor >( the_main.primary_config() );
 
+        // run each phase of the operation, while checking for errors
+        if ( ! the_monitor->start() || 
+             ! the_monitor->listen() ||
+             ! the_monitor->stop() )
+        {
+            scarab::signal_handler::cancel_all( RETURN_ERROR );
+        }
+
+        // brief pause to ensure that the cancellation goes through
+        std::this_thread::sleep_for( std::chrono::milliseconds(100) );
+      
+        the_return = scarab::signal_handler::get_exited() ? 
+                scarab::signal_handler::get_return_code() : dl_success().rc_value() / 100;  
+    };
+
+    the_main.callback( t_callback );
+*/
     // Parse CL options and run the application
-//    CLI11_PARSE( the_main, argc, argv );
+    CLI11_PARSE( the_main, argc, argv );
 
-    
 
-        scarab::param_node config = scarab::param_node();
-//        return Nymph::RunNymph(  config_test->as_node() );
-        return Nymph::RunNymph(  config );
+        return Nymph::RunNymph(  the_main.primary_config() );
+//---------------------End Tests-------------------------------------
     }
     catch( std::exception& e )
     {
