d2d3fd6917de1c1a62de5be1c1812a8547e804ad
Noah Oblath <noah.oblath@pnnl.gov>
2022-06-14 23:46:27 -0400

    Add NymphTesting pybind build

---
+++ Testing/Python/Bindings/NymphTestingPybind.cc
@@ -0,0 +1,18 @@
+/*
+ * NymphTestingPybind.cc
+ *
+ *  Created on: Jun 14, 2022
+ *      Author: N.S. Oblath
+ */
+
+#include <pybind11/pybind11.h>
+
+
+namespace py = pybind11;
+
+
+PYBIND11_MODULE(_nymph_testing, nymphTestingPackage) {
+
+    nymphTestingPackage.doc() = "Nymph Testing package";
+
+}
