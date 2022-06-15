ef6e0dce11b067ec9f17b1bf05afd2311630b8b2
Florian Thomas <flthomas@students.uni-mainz.de>
2022-06-15 17:27:00 -0400

    Add missing bindings for SignalBase

--- Python/Bindings/Processor/SignalPybind.hh
+++ Python/Bindings/Processor/SignalPybind.hh
@@ -21,23 +21,49 @@ namespace py = pybind11;
 namespace NymphPybind
 {
     
+    //trampoline class
+    class PySignalBase : public Nymph::SignalBase {
+        public:
+            /* Inherit the constructors */
+            using Nymph::SignalBase::SignalBase;
+
+            /* Trampoline (need one for each virtual function) */
+            void Connect( Nymph::SlotBase* slot, int group = -1 ) override {
+                PYBIND11_OVERRIDE_PURE(
+                    void, /* Return type */
+                    Nymph::SignalBase,      /* Parent class */
+                    Connect,          /* Name of function in C++ (must match Python name) */
+                    slot,
+                    group/* Argument(s) */
+                );
+            }
+    };
+    
+    void ExportSignalBase( py::module_& nymphProcessor)
+    {
+
+       py::class_< Nymph::SignalBase, PySignalBase, std::shared_ptr<Nymph::SignalBase > >(nymphProcessor, "_SignalBase")
+                .def(py::init<const std::string& >())
+                .def("Connect", &Nymph::SignalBase::Connect)
+                .def("disconnect", &Nymph::SignalBase::Disconnect)
+                .def("disconnect_all", &Nymph::SignalBase::DisconnectAll)
+                .def_property_readonly("connections", static_cast< std::set< Nymph::SlotBase* >& (Nymph::SignalBase::*)() const>(&Nymph::SignalBase::Connections))
+                .def_property("name", static_cast< const std::string& (Nymph::SignalBase::*)() const>(&Nymph::SignalBase::Name),
+                                      [](Nymph::SignalBase& signal, const std::string& name){signal.Name() = name;} )
+                .def_property("do_breakpoint", &Nymph::SignalBase::GetDoBreakpoint, &Nymph::SignalBase::SetDoBreakpoint);
+        
+    }
+    
     template< typename... XArgs >
     void ExportSignal( py::module_& nymphProcessor, const std::string& typestr)
     {
         std::string pyClsName = std::string("_Signal") + typestr;
 
-       py::class_< Nymph::Signal< XArgs... >, std::shared_ptr<Nymph::Signal< XArgs... > > >(nymphProcessor, pyClsName.c_str())
+       py::class_< Nymph::Signal< XArgs... >, Nymph::SignalBase, std::shared_ptr<Nymph::Signal< XArgs... > > >(nymphProcessor, pyClsName.c_str())
                 .def(py::init<const std::string& >())
                 .def(py::init<const std::string&,  Nymph::Processor* >())
                 .def("emit", &Nymph::Signal< XArgs... >::Emit)
-                .def("__call__", &Nymph::Signal< XArgs... >::operator())
-                .def("connect", &Nymph::Signal< XArgs...>::Connect)
-                .def("disconnect", &Nymph::Signal< XArgs... >::Disconnect)
-                .def("disconnect_all", &Nymph::Signal< XArgs... >::DisconnectAll)
-                .def_property_readonly("connections", static_cast< std::set< Nymph::SlotBase* >& (Nymph::Signal< XArgs... >::*)() const>(&Nymph::Signal< XArgs... >::Connections))
-                .def_property("name", static_cast< const std::string& (Nymph::Signal< XArgs... >::*)() const>(&Nymph::Signal< XArgs... >::Name),
-                                      [](Nymph::Signal< XArgs... >& signal, const std::string& name){signal.Name() = name;} )
-                .def_property("do_breakpoint", &Nymph::Signal< XArgs... >::GetDoBreakpoint, &Nymph::Signal< XArgs... >::SetDoBreakpoint);
+                .def("__call__", &Nymph::Signal< XArgs... >::operator());
         
     }
 
