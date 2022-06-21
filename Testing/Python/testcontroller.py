04c54f3dc57c7e8ef9846f83a5d41a1b7581bdf3
Pranava Teja Surukuchi <pranavateja.surukuchi@yale.edu>
2022-06-21 18:57:24 -0400

    Small mods to testcontroller.py

--- Testing/Python/testcontroller.py
+++ Testing/Python/testcontroller.py
@@ -9,53 +9,28 @@ import unittest
 
 import _nymph
 
-'''Data class with integer variables'''
 class TestController(_nymph.control._Controller):
     
     def __init__(self, test_var1=5):
         super().__init__()
         self.test_var1 = test_var1
 
-    def SetBreakFlag(flag):
-        self.B=
+    def set_break_flag(flag):
+        self.fBreakFlag=flag
 
+    def set_canceled(flag):
+        f_canceled.store(flag)
+        
+    def do_cancellation(flag):
+        _Controller.do_cancellation(code)
 
 class TestControllerMethod(unittest.TestCase):
     controller=TestController()
     def test_idata_assignment(self):
-        self.assertEqual(self.data1.test_var1,0)
-
+        self.controller.SetCycleTimeMS( 100 )
+        self.assertEqual(controller.GetCycleTimeMS(),0)
+        
 if __name__ == '__main__':
     unittest.main()
 
-
-
-
-namespace Nymph
-{
-    class ControllerRevealer : public Controller
-    {
-        public:
-            using Controller::Controller;
-            virtual ~ControllerRevealer() {}
-
-            void SetBreakFlag( bool flag )
-            {
-                fBreakFlag = flag;
-                return;
-            }
-
-            void SetCanceled( bool flag )
-            {
-                f_canceled.store( flag );
-                return;
-            }
-
-            void do_cancellation( int code )
-            {
-                Controller::do_cancellation( code );
-                return;
-            }
-    };
-}
 
