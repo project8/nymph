f89f06307c38300242a9590ada4269baaacea196
Noah Oblath <nsoblath@gmail.com>
2025-09-09 15:32:15 -0700

    Flesh out cmake command in CppRepo and add CppRepo build to build.yaml

--- Examples/CppRepo/BUILD_ME.sh
+++ Examples/CppRepo/BUILD_ME.sh
@@ -3,6 +3,12 @@
 mkdir build
 cd build
 
-cmake ..
+cmake -DCMAKE_BUILD_TYPE=${Nymph_BUILD_TYPE:=Debug} \
+    -DNymph_BUILD_NYMPH_EXE=${Nymph_BUILD_NYMPH_EXE:=FALSE} \
+    -DNymph_ENABLE_EXECUTABLES=${Nymph_ENABLE_EXECUTABLES:=TRUE} \
+    -DNymph_ENABLE_PYTHON=${Nymph_ENABLE_PYTHON:=FALSE} \
+    -DNymph_ENABLE_TESTING=${Nymph_ENABLE_TESTING:=FALSE} \
+    -DNymph_SINGLETHREADED=${Nymph_SINGLETHREADED:=FALSE} \
+    ..
 
-make -j${NARG} install
+make -j${NARG:=} install
